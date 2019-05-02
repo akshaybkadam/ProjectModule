dba4becf376a51cf47587b168e6222f267a44804
akshaybkadam <39439103+akshaybkadam@users.noreply.github.com>
2019-05-02 06:55:08 +0530

    adding some js code

---
+++ WebContent/JS/loginData.js
@@ -0,0 +1,11 @@
+function saveEmployee(){
+	/**
+	 * trying to get all values by the form id*/
+	
+	var uname = document.getElementById("name").value;
+	var email = document.getElementById("email").value;
+	var password = document.getElementById("password").value;
+	
+	alert(email);
+	
+}
